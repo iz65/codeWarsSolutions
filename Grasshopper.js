3cd132323ce8cebb153855098eeccccf5370a77d
igorzhilin <zhilin.igor@gmail.com>
2020-08-15 11:02:41 -0400

    Add codewar solution

---
+++ Grasshopper.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/55cbd4ba903825f7970000f5/solutions/javascript
+
+function getGrade (s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
+}
